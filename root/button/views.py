446694ff1b3ec1300d9007800d97de708ff0fd8f
Baudrillard <smbalf+222@gmail.com>
2023-01-19 15:19:18 +0000

    save on route change
    implementing sessions for more savings

--- root/button/views.py
+++ root/button/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_socketio import emit
 from flask_login import current_user, login_required
 
@@ -14,15 +14,21 @@ def msg_callback():
 @button.route("/click")
 @login_required
 def beta_button():
-    return render_template("button/button.html", user_id = str(current_user.id))
+    return render_template("button/button.html", 
+                            user_id = str(current_user.id), 
+                            user_gold = current_user.gold)
 
 @socketio.on("update_gold")
 def update_gold(data):
     user_id = data["user_id"]
+    user_gold = data["user_gold"]
     user = User.objects(id=user_id).first()
-    user.gold += 1
-    user.save()
-    emit("update_gold_response", {"gold": user.gold}) # removed callback=msg_callback as worked fine..
-    print(user.gold)
+    user.gold = user_gold
+    if not request.path == "/click":
+        user.save()
+        print("SAVING DATA DUE TO DIFFERENT PATH")
+        pass
 
-
+@socketio.on('disconnect')
+def save_on_dc():
+    print("SAVING DUE TO LOGOUT")
