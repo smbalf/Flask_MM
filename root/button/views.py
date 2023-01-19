2eff968511afe45511f31cb8f6f1a03b1d95b78c
Baudrillard <smbalf+222@gmail.com>
2023-01-19 19:20:37 +0000

    slight improvement
    to do:
    - save on page change
    - save on timer
    - implement session on login

--- root/button/views.py
+++ root/button/views.py
@@ -8,6 +8,7 @@ from root.globals import socketio
 button = Blueprint("button", __name__)
 
 def msg_callback():
+    """For use with emit"""
     print("MESSAGE RECEIVED BY CLIENT CONFIRMED!")
 
 
@@ -26,20 +27,21 @@ def update_gold(data):
     user = User.objects(id=user_id).first()
     user.gold = user_gold
     session[user_sid] = user
-    print(session[user_sid])
-    print(session)
-    if not request.path == "/click":
+    """if request.namespace != "/update_gold":
         user.save()
-        print("SAVING DATA DUE TO DIFFERENT PATH")
+        print("SAVING DATA DUE TO DIFFERENT PATH")"""
 
 @socketio.on('disconnect')
 @login_required
 def save_on_dc():
     user_sid = str(current_user.id)
-    if user_sid in session['_user_id']:
-        user = session[user_sid]
-        user.save()
-        print("SAVING DUE TO LOGOUT")
-    else:
-        print("FAILED TO SAVE ON DC")
-        print(current_user.id)
+    try:
+        if user_sid in session['_user_id']:
+            user = session[user_sid]
+            user.save()
+            print("SAVING DUE TO LOGOUT")
+        else:
+            print("FAILED TO SAVE ON DC")
+            print(current_user.id)
+    except KeyError:
+        print("KeyError - user session not saved yet")
